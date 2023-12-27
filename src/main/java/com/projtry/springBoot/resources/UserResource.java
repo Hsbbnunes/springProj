87f8f847b1735c98d445c805a832dfa7b0569b5d
Henrique Nunes <hsbbnunes@gmail.com>
2023-12-27 17:19:39 +0000

    Servico e findById

--- src/main/java/com/projtry/springBoot/resources/UserResource.java
+++ src/main/java/com/projtry/springBoot/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.projtry.springBoot.resources;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +11,25 @@ import com.projtry.springBoot.entities.User;
 import com.projtry.springBoot.services.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
     
+    @Autowired
     private UserService service;
 
     @GetMapping
     public ResponseEntity <List <User>> findAll() {
         List <User> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity <User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
     
 }
