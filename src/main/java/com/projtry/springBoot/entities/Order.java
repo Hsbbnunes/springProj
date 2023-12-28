3844d6e418173a65162ab04828110d7bd1d05a0f
Henrique Nunes <hsbbnunes@gmail.com>
2023-12-28 10:11:01 +0000

    Timezone + Instant

--- src/main/java/com/projtry/springBoot/entities/Order.java
+++ src/main/java/com/projtry/springBoot/entities/Order.java
@@ -3,6 +3,9 @@ package com.projtry.springBoot.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +15,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb-order")
+@Table(name = "tb_order")
 public class Order implements Serializable{
 
     private static final long serialVersionUID = 1L;
@@ -20,11 +23,16 @@ public class Order implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order() {
+    }
 
     public Order(Long id, Instant moment, User client) {
         this.id = id;
